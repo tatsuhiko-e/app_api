3a5f050e9663ffd60f59a464f8d7b0e7951ef4da
tatsuhiko-e <70308705+tatsuhiko-e@users.noreply.github.com>
2022-10-31 12:45:19 +0900

    add admin tabel

---
+++ app/controllers/api/v1/admin/registrations_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::Admin::RegistrationsController < DeviseTokenAuth::RegistrationsController
+  private
+
+  def sign_up_params
+    params.permit(:email, :password, :password_confirmation, :team_name)
+  end
+
+end
---
+++ test/controllers/api/v1/admin/registrations_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Api::V1::Admin::RegistrationsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
